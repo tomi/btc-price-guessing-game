1e545a9b6d324da930dde55e6e006a5032dc8bdc
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2022-11-27 22:42:50 +0200

    rename GuessDirection enum to Direction

--- apps/backend/src/domain/guess.ts
+++ apps/backend/src/domain/guess.ts
@@ -1,13 +1,13 @@
 import { Iso8601Timestamp, Uuid } from "./common";
 
-export enum GuessDirection {
+export enum Direction {
   Up = "up",
   Down = "down",
 }
 
 export interface Guess {
   playerId: Uuid;
-  direction: GuessDirection;
+  direction: Direction;
   priceWhenCreated: number;
   createdAt: Iso8601Timestamp;
 }
